309addb4d5c5af662438b20baa85fab9901b73ed
LiguoXia <xialiguo0212@email.com>
2020-09-10 22:14:48 +0800

    feat:添加mybatis-plus等依赖

--- demo-auth/src/main/java/com/liguo/demo/auth/DemoAuthApplication.java
+++ demo-auth/src/main/java/com/liguo/demo/auth/DemoAuthApplication.java
@@ -1,13 +1,30 @@
 package com.liguo.demo.auth;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
+@Slf4j
 public class DemoAuthApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoAuthApplication.class, args);
+		long startTime = System.currentTimeMillis();
+		SpringApplication app = new SpringApplication(DemoAuthApplication.class);
+		app.setBannerMode(Banner.Mode.OFF);
+		app.run(args);
+		log.info("^-^####################################################^-^");
+		log.info("^-^##### Auth service was started,{} senconds.#####^-^", System.currentTimeMillis() - startTime);
+		log.info("^-^####################################################^-^");
+	}
+
+	@GetMapping("/status")
+	public String status() {
+		return "Service is ok";
 	}
 
 }
--- demo-getway/src/main/java/com/liguo/demo/getway/DemoGetwayApplication.java
+++ demo-getway/src/main/java/com/liguo/demo/getway/DemoGetwayApplication.java
@@ -1,13 +1,30 @@
 package com.liguo.demo.getway;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@Slf4j
 @SpringBootApplication
 public class DemoGetwayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoGetwayApplication.class, args);
+		long startTime = System.currentTimeMillis();
+		SpringApplication app = new SpringApplication(DemoGetwayApplication.class);
+		app.setBannerMode(Banner.Mode.OFF);
+		app.run(args);
+		log.info("^-^####################################################^-^");
+		log.info("^-^##### Getway service was started,{} senconds.#####^-^", System.currentTimeMillis() - startTime);
+		log.info("^-^####################################################^-^");
+	}
+
+	@GetMapping("/status")
+	public String status() {
+		return "Service is ok";
 	}
 
 }
