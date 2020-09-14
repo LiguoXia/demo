31fc01fad6032a3f4cb3bc2677af94a620c93315
xialiguo <xialiguo0212@gmail.com>
2020-09-14 13:54:06 +0800

    fix:swagger 扫描路径修改

--- demo-consume/src/main/java/com/liguo/demo/consume/config/SwaggerConfig.java
+++ demo-consume/src/main/java/com/liguo/demo/consume/config/SwaggerConfig.java
@@ -27,7 +27,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.liguo.demo.core.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.liguo.demo.consume.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
